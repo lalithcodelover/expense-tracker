a6bcdaf52e6821b625cfc15ce4e9d617fdc66cbc
lalithcodelover <lalith.vlr@gmail.com>
2022-11-27 00:06:42 +0530

    update profile added

--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -10,10 +10,17 @@ const fullName = nameInputRef.current.value
 const profilePhoto = profilePhotoRef.current.value
 
 axios.post('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCZ_ansWvG2rYiw6QgB7Xm5LUQahA4kH0A',{
-    idToken= localStorage.getItem('token'),
+    idToken : localStorage.getItem('token'),
     displayName:fullName,
     photoUrl:profilePhoto,
     returnSecureToken:true
+})
+.then(res=>{
+    console.log(res.data);
+    
+})
+.catch(err=>{
+    console.log(err.message)
 })
     }
   return (
@@ -24,13 +31,13 @@ axios.post('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSy
       </div>
       <div className={classes.box}>
       <h3>Contact Details</h3>
-      <button>Cancel</button>
+      <button className={classes.cancelbutton}>Cancel</button>
         <form onSubmit={updateProfileHandler} className={classes.updateform}>
             
             <label htmlFor="name">Full Name</label>
-            <input type="text" />
+            <input type="text" ref={nameInputRef}/>
             <label htmlFor="profilephoto">Profile Photo URL</label>
-            <input type="text" />
+            <input type="text" ref={profilePhotoRef}/>
             <button  className={classes.updatebutton}>Update</button>
         </form>
       </div>
