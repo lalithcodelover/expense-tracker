6fcb1a05d38e1e716660fa85269db1b54f1a7d91
lalithcodelover <lalith.vlr@gmail.com>
2022-11-27 16:33:48 +0530

    logout button added

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import classes from './Header.module.css'
 const Header = () => {
+const history = useHistory()
+  const logoutHandler=()=>{
+
+    localStorage.removeItem('token')
+
+    history.replace('/login')
+  }
   return (
     <div className={classes.header}>
       <h1>Expense Tracker</h1>
+      <button onClick={logoutHandler} className={classes.logoutbutton}>Logout</button>
     </div>
   )
 }
