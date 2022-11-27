23cdeea3a73b2cf6560e58304505cf3ba11ffd1f
lalithcodelover <lalith.vlr@gmail.com>
2022-11-27 16:15:22 +0530

    verify email added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
-import classes from './Welcome.module.css'
+import axios from "axios";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import classes from "./Welcome.module.css";
 const Welcome = () => {
-    const history = useHistory()
-    const profileUpdateHandler=()=>{
-         history.push('/updateprofile')
-    }
+  const history = useHistory();
+
+  const profileUpdateHandler = () => {
+    history.push("/updateprofile");
+  };
+
+  const verifyEmailHandler = () => {
+    const token = localStorage.getItem("token");
+
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCZ_ansWvG2rYiw6QgB7Xm5LUQahA4kH0A",
+        {
+          requestType: "VERIFY_EMAIL",
+          idToken: token,
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        alert("Login your account before veryfying.");
+
+        console.log(err.code);
+      });
+  };
   return (
     <div className={classes.welcome}>
-        
-      <h1 >Welcome to Expense Tracker</h1>
-      <h3>Your profile is incomplete. <span onClick={profileUpdateHandler}>Complete Now.</span></h3>
+      <h1>Welcome to Expense Tracker</h1>
+      <h3>
+        Your profile is incomplete.{" "}
+        <span onClick={profileUpdateHandler}>Complete Now.</span>
+      </h3>
+      <button onClick={verifyEmailHandler} className={classes.verifybutton}>
+        Verify your Email
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
